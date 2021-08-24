9371f9a69d4c594f31d55bc0db2331e47e8d85c2
kmtym1998 <kmtym.devloper@gmail.com>
2021-08-25 08:34:45 +0900

    postのエンドポイント追加

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// Graphqlレスポンスっぽいの
+router.post('/graphql', (req, res) => {
+  if (!req.body.text)
+    return res.status(400).json({
+      msg: 'request bodyにtextを含めてください',
+    });
+  return res.json({
+    data: {
+      me: {
+        store: {
+          tenant: {
+            id: 'user-id',
+            text: req.body.text,
+            privileged: true,
+          },
+        },
+      },
+    },
+  });
 });
 
 module.exports = router;
